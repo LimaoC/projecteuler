4bc6452cee2582343cefa2636c1b186ca0a32c2e
LimaoC <limao.chang@outlook.com>
2021-04-10 16:39:55 +1000

    Updating docstrings

--- 15 Lattice Paths.py
+++ 15 Lattice Paths.py
@@ -1,7 +1,8 @@
-# Starting in the top left corner of a 2×2 grid, and only being able to move to
-# the right and down, there are exactly 6 routes to the bottom right corner.
-# How many such routes are there through a 20x20 grid?
-
+"""
+Starting in the top left corner of a 2×2 grid, and only being able to move to
+the right and down, there are exactly 6 routes to the bottom right corner. How
+many such routes are there through a 20x20 grid?
+"""
 import math
 
 N = 20
